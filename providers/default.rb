33e7d5fff0cdb00139f163313405dda22aba2d0b
Martin Fenner <mfenner@plos.org>
2015-08-06 07:21:24 +0200

    create application root if it doesn't exist

--- providers/default.rb
+++ providers/default.rb
@@ -21,6 +21,16 @@ action :load do
   filename = new_resource.dotenv == "default" ? ".env" : ".env.#{new_resource.dotenv}"
   filepath = "/var/www/#{new_resource.name}/shared/#{filename}"
 
+  # create application root folder, subfolders, and set permissions if they don't exist
+  %W{ #{new_resource.name} #{new_resource.name}/shared #{new_resource.name}/shared/public }.each do |dir|
+    directory "/var/www/#{dir}" do
+      owner new_resource.user
+      group new_resource.group
+      mode '0755'
+      recursive true
+    end
+  end
+
   # create .env file from template if we don't find it
   template filepath do
     source ".env.erb"
