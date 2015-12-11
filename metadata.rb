0343aaa39aaed6a91b05211cd3a2ae9da95e89ba
Martin Fenner <martin.fenner@datacite.org>
2015-12-11 07:53:00 -0500

    optionally use capistrano folders

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name              "dotenv"
 maintainer        "Martin Fenner"
-maintainer_email  "martin.fenner@datacite.org"
+maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Installs dotenv gem and reads ENV variables"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.2.14"
+version "1.0.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
